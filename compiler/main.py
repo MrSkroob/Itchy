818ce71e1377c529e99798bbed9c159ec61b6112
Skrubunger <yaccoby101@gmail.com>
2026-07-08 22:15:30 +0100

    remove unused imports

--- compiler/main.py
+++ compiler/main.py
@@ -1,5 +1,4 @@
 from parser import Parser
-from tokenizer import Tokenizer, Definitions
 from itch_ast import build_ast
 from tools.ast_printer import print_ast
 
@@ -7,10 +6,6 @@ from tools.ast_printer import print_ast
 parser = Parser()
 
 with open("input/code.txt") as f:
-    # for token in Tokenizer(Definitions, {"Whitespace"}).read(f.read()):
-    #     print(token.literal)
     result = parser.read(f.read())
-    # print(result)
-    # print_ast(result)
     tree = build_ast(result.tree)
     print_ast(tree)
