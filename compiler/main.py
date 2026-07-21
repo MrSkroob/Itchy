69e5e9bb930e4c7f925bd4e3bba8babe6b35803e
Skrubunger <yaccoby101@gmail.com>
2026-07-21 18:01:30 +0100

    add compile method to main

--- compiler/main.py
+++ compiler/main.py
@@ -3,21 +3,24 @@ from itch_ast import build_ast
 from tools.ast_printer import print_ast
 from assembler import Assembler
 
-from tokenizer import Tokenizer, Definitions
-
 
 parser = Parser()
 assembler = Assembler()
 
 print("Running")
 
-with open("input/code.txt") as f:
-    # tokenizer = Tokenizer(Definitions, {"Whitespace", "Newline"})
-    # for token in tokenizer.read(f.read()):
-    #     print(token.literal, token.kind.name)
 
-    result = parser.read(f.read())
-    tree = build_ast(result.tree)
-    assembler.assemble(tree, "output/Scratch Project.sb3", "Sprite1")
-    
+def compile(code: str, output: str, target: str):
+    """
+    Compiles code to an existing output .sb3 file, targetting a specific sprite/stage
+    """
+    parsed = parser.read(code)
+    tree = build_ast(parsed.tree)
+    assembler.assemble(tree, output, target)
     print_ast(tree)
+
+
+with open("input/code.txt") as f:
+
+    compile(f.read(), "output/Scratch Project.sb3", "Sprite1")
+    
