9b350a39d10a86ae2cc2562a4a3022b25a3fbd97
Skrubunger <yaccoby101@gmail.com>
2026-09-03 19:24:58 +0100

    remove control_delete_this_clone from stage_blocks

--- src/itchy/scratch_blocks.py
+++ src/itchy/scratch_blocks.py
@@ -162,7 +162,6 @@ STAGE_BLOCKS: dict[str, Block | Reporter | Event] = {
     "control_wait_until": Block((ReturnType("CONDITION"),)),
     "control_stop": Block(fields=(Field("STOP_OPTION", ("this script", "all", "other scripts in sprite")),)), # this is basically the return block. though we'll need to figure out how to return variables.
     "control_create_clone_of": Block((Menu("control_create_clone_of_menu", "CLONE_OPTION", expected=("_myself_",)),)),
-    "control_delete_this_clone": Block(()),
 
     # --- sensing ----------------------------------------------------
     "sensing_askandwait": Block((ReturnType("QUESTION", DataType.STRING),)),
