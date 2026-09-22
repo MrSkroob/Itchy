bf4a66e7cc6f7a0e402433aa3d7d8343bcd5b0f0
Jacob Liu <3070970l@student.gla.ac.uk>
2026-09-22 16:44:06 +0100

    reset expected on error

--- src/itchy/parserv2.py
+++ src/itchy/parserv2.py
@@ -289,6 +289,7 @@ class Parser():
                 if self.allow_insertions:
                     if isinstance(part, Terminal):
                         if part.child.name in self.recovery_nodes:
+                            result.expected = result.expected.create_and_reset()
                             self.accumulated_errors.append(result)
                             children.extend(self.recovery_nodes[part.child.name]())
                             continue
